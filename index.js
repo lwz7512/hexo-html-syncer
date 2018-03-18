5c8ecae97b5a77253c8ce0f23b0ef3d379beb35b
lwz7512 <lwz7512@gmail.com>
2018-03-18 20:33:44 +0800

    add post create...

--- index.js
+++ index.js
@@ -3,19 +3,17 @@
 var fs = require('fs');
 var path = require('path');
 
-var url = 'http://mp.weixin.qq.com/s/Tp4W-_0MXMvN5Tw3jlrtLg';
-var markdownPath = path.join(__dirname, 'markdown');
-var articlesPath = path.join(__dirname, 'articles');
-
-var articlesExist = fs.existsSync(articlesPath);
-if(!articlesExist) fs.mkdirSync(articlesPath);
-var markdownExist = fs.existsSync(markdownPath);
-if(!markdownExist) fs.mkdirSync(markdownPath);
-
-// require('./lib/convertor')(url, markdownPath, articlesPath);
+// var url = 'http://mp.weixin.qq.com/s/Tp4W-_0MXMvN5Tw3jlrtLg';
 
 hexo.extend.console.register('syncer', 'Display configuration', function(args){
   console.log('syncer is running .....');
-});
+  if(!args._.length) return console.log('NO artile url provided!');
 
-// ...
+  var url = args._[0];
+  require('./lib/convertor')(url)
+  .then(result =>{
+    // console.log('markdown file created?');
+    require('./lib/post')(hexo, result.title, result.markdown);
+  });
+
+});
