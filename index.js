8ceec2c462485de44457ba6fd9f2a2871367f083
lwz7512 <lwz7512@gmail.com>
2018-03-19 13:28:33 +0800

    complete first version of syncer!

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 
-// var url = 'http://mp.weixin.qq.com/s/Tp4W-_0MXMvN5Tw3jlrtLg';
+// var url = 'https://www.meipian.cn/15x3nh7m'; //@2018/03/16 - 可以卑微如尘，不可扭曲如蛆虫
+// var url = 'http://mp.weixin.qq.com/s/Tp4W-_0MXMvN5Tw3jlrtLg'; //@2018/03/17 - 黎明守夜人
+// var url = 'http://mp.weixin.qq.com/s/Uuj60qopwWicngpEbZcNRQ'; //@2018/03/19 - 二月二龙抬头，以万物为刍狗
 
-hexo.extend.console.register('syncer', 'Display configuration', function(args){
-  console.log('syncer is running .....');
-  if(!args._.length) return console.log('NO artile url provided!');
-
-  var url = args._[0];
-  require('./lib/convertor')(url)
-  .then(result =>{
-    // console.log('markdown file created?');
-    require('./lib/post')(hexo, result.title, result.markdown);
-  });
-
-});
+hexo.extend.console.register('syncer', 'Download article by url, then conver to post', {
+  desc: 'request the article then process to simple version of markdown, also images save to local.',
+  usage: 'hexo syncer <url of article>',
+}, require('./lib/core').bind(hexo));
